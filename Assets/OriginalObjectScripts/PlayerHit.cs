35ccf68de7b15a98df0eb51940851d18550d0b1e
平井将也 <>
2018-05-06 21:57:02 +0900

    [add]プレイヤーへの攻撃判定追加
    #5 ゲームの進行を考え操作キャラがダメージを受けるように攻撃判定追加

---
+++ Assets/OriginalObjectScripts/PlayerHit.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+[RequireComponent (typeof (Rigidbody))]
+public class PlayerHit : MonoBehaviour {
+ 
+    public float power, time;
+    public GameObject damageEffect;
+    public Player pc;
+ 
+    protected override void Start () {
+        if (time != 0) {
+            StartCoroutine(DestroyHit());
+        }
+    }
+ 
+    protected virtual IEnumerator DestroyHit(){
+        yield return new WaitForSeconds(time);
+        Destroy(gameObject);
+    }
+ 
+    protected virtual void OnTriggerEnter(Collider c){
+        if (TagUtility.getParentTagName(character.getGameObject().tag) == "Player") {
+            if (TagUtility.getParentTagName(c.gameObject) == "Enemy") {
+                c.GetComponent<Enemy>().damage(power);
+                Instantiate(damageEffect, transform.position, Quaternion.identity);
+            }
+        }
+    }
+}
