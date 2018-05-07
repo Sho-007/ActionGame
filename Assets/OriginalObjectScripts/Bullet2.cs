61cd763cc2394fb06833b258c64a1174e810716b
平井将也 <>
2018-05-08 01:21:12 +0900

    [change&add]エネミーの弾の発射機能変更と追加
    #5 エネミーが弾の発射をする際に操作キャラの移動に合わせて撃つように変更して弾の発射のコードを追加

---
+++ Assets/OriginalObjectScripts/Bullet2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet2 : MonoBehaviour {
+	public float
+
+	// Use this for initialization
+	void Start () {
+		//弾を3秒後に発射する
+		Destroy (this.gameObject,3);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//弾を移動
+		transform.Translate (transform.forward * Time.deltaTime * speed);
+		
+	}
+
+	 if (Input.GetKeyDown("space"))
+    {
+		//runcherbulletにbulletのインスタンスを格納
+        GameObject runcherBullet = GameObject.Instantiate(bullet) as GameObject; 
+		//アタッチしているオブジェクト前方にbullet speedの速さで発射
+        runcherBullet.GetComponent<Rigidbody>().velocity = transform.forward * bulletSpeed;
+        runcherBullet.transform.position = transform.position;
+	}
+}
+
