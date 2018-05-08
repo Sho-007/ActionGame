7c475e84c1b194000db381b53d2a8935d93df942
平井将也 <>
2018-05-09 01:07:40 +0900

    [add]エネミーに自動追尾機能追加
    #7 エネミーが操作キャラを自動追尾する機能を追加

---
+++ Assets/OriginalObjectScripts/Chase.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//追加する要素
+using UnityEngine.AI;
+
+public class Chase : MonoBehaviour {
+	public Transform target;
+	public Transform[] targets;
+	public NavMeshAgent agent;
+	public 
+
+	public GameObject target;
+	private NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//ターゲットの位置を目的地に設定する。
+		agent.destination = target.transform.position;
+	}
+}
