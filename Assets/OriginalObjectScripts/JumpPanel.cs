6c8fde177ce395a33319935141153458a1737b26
平井将也 <>
2018-05-09 01:06:14 +0900

    [add]大きくジャンプさせるパネルを追加
    #6 操作キャラが踏んだ際に大きくジャンプ移動させるパネルを追加

---
+++ Assets/OriginalObjectScripts/JumpPanel.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wind : MonoBehaviour {
+
+	public float Player_Jump;
+	public bool Jump;
+
+	GameObject player;
+	
+	void Update () {
+
+		player = GameObject.Find ("Player");
+	
+	}
+	
+	void OnTriggerEnter2D(Collider2D col){
+	
+		if (col.gameObject.tag == "Player") {
+		  if (Jump) {
+		    player.GetComponent<Rigidbody2D> ().AddForce (transform.up * Player_Jump * Time.deltaTime, ForceMode2D.Impulse);
+			}
+		}
+	}
+}
