c6d65d5f53e2b5eb805e6815f5e16be2125d830b
平井将也 <>
2018-05-09 01:03:29 +0900

    [add] 操作キャラへの機能追加
    #4 操作キャラの行動によって道同士を繋げる機能の追加

---
+++ Assets/OriginalObjectScripts/AddForceObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddForceObject : MonoBehaviour {
+
+//　Blockレイヤーを持つゲームオブジェクトと接触したら力を加える
+	void OnControllerColliderHit(ControllerColliderHit col) {
+		if(col.gameObject.layer == LayerMask.NameToLayer ("Block")) {
+			col.rigidbody.AddForce(-col.normal * 100f);
+		}
+	}
+}
+ 
