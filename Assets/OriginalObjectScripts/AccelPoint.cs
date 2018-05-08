12f7b666f84f8f91d725743f1a360e36d179736b
平井将也 <>
2018-05-09 01:04:48 +0900

    [add] 移動速度を上げるパネルの追加
    #5 操作キャラが踏んだ際に移動速度を上げるパネルを追加

---
+++ Assets/OriginalObjectScripts/AccelPoint.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class AccelPoint : MonoBehaviour {
+ 
+    void OnTriggerEnter(Collider other){
+        other.gameObject.GetComponent&lt;Rigidbody&gt;().AddForce(new Vector3(0,0,10),ForceMode.VelocityChange);
+    }
+}
