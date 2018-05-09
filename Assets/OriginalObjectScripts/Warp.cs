da03f3f3156fb5b49a4c2e2c61b395a450b4e45a
平井将也 <>
2018-05-10 01:34:55 +0900

    [add]ステージのオブジェクト追加
    #7 ステージにエリアとエリアを繋ぐワープ装置を追加

---
+++ Assets/OriginalObjectScripts/Warp.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warp : MonoBehaviour {
+	public Transform WarpPanel;
+
+	voic OnTriggerEnter(Collider col) {
+		if(col.tag == "Player"){
+			//キャラクターの状態をワープ状態に変更
+			col.gameObject.<WarpChara>().SetDate(WarpChara.WarpCharaState.goToWarpPoint,transform,WarpPanel = new Vector3(0,20,0);
+
+		}
+	}
+}
