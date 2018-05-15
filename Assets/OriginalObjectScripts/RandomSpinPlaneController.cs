3a3f7e82cfa13a1f8fbde95bd6b03eb82620180b
平井将也 <>
2018-05-15 22:10:22 +0900

    [hotfix]オブジェクトにアタッチできないバグの修正
    #1 スペルの打ち間違いや、速度の基準を修正

--- Assets/OriginalObjectScripts/RandomSpinPlaneController.cs
+++ Assets/OriginalObjectScripts/RandomSpinPlaneController.cs
@@ -1,16 +1,17 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RandomSpinPlaneController : MonoBehaviour {
-	public float;
+	public float t;
 	public Vector3 centor;
 	public Vector3 axis;
 
 	// Use this for initialization
 	void Start () {
 		t = 0f;
-		center = Vector3.zero; // ワールド座標の原点の周りに回転
+		centor = Vector3.zero; // ワールド座標の原点の周りに回転
 		axis = Vector3.up;     // Y軸の周りに回転
 		
 	}
@@ -19,9 +20,9 @@ public class RandomSpinPlaneController : MonoBehaviour {
 	void Update () {
 		if (t < 1f) {
       float prevT = t;
-      t = Math.min(1f, t + Time.deltaTime); // 360度を超えないように
+      t = Math.Min(1f, t + Time.deltaTime); // 360度を超えないように
       float dt = t - prevT;
-      transform.RotateAround(center, axis, 360 * dt);
+      transform.RotateAround(centor, axis, 360 * dt);
     }
 	}
 }
