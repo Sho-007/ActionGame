08b91045dc45049b5e04a1e1fa7d5be1995b582f
平井将也 <>
2018-05-19 00:19:32 +0900

    [add]ワープする際の演出を追加
    #4 操作キャラがワープする際の演出を追加

--- Assets/OriginalObjectScripts/Warp.cs
+++ Assets/OriginalObjectScripts/Warp.cs
@@ -2,14 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Warp : MonoBehaviour {
-	public Transform WarpPanel;
+[RequireComponent( typeof( NavMeshAgent ) )]
+public class  Warp :Warp 
+{
+	[SerializeField]
+	private float   m_scaleTime = 0.0f;
+	[SerializeField]
+	private float   m_waitTime  = 0.0f;
 
-	voic OnTriggerEnter(Collider col) {
-		if(col.tag == "Player"){
-			//キャラクターの状態をワープ状態に変更
-			col.gameObject.<WarpChara>().SetDate(WarpChara.WarpCharaState.goToWarpPoint,transform,WarpPanel = new Vector3(0,20,0);
+	protected override IEnumerator OffMeshLinkProcess( Vector3 i_targetPos )
+	{
+		Vector3 defaultScale    = transform.localScale;
 
-		}
+		yield return new WaitForSeconds( m_waitTime );
+
+		yield return ScaleProcess( Vector3.zero, m_scaleTime );
+
+		yield return new WaitForSeconds( m_waitTime );
+
+		transform.position  = i_targetPos;
+
+		yield return ScaleProcess( defaultScale, m_scaleTime );
+
+		yield return new WaitForSeconds( m_waitTime );
 	}
-}
+
+	private IEnumerator ScaleProcess( Vector3 i_toScale, float i_time )
+	{
+		Vector3 fromScale = transform.localScale;
+
+		float startTime = Time.time;
+		float endTime   = startTime + i_time;
+
+		while( Time.time < endTime )
+		{
+			float elapsedTime = Time.time - startTime;
+			float elapsedRate = elapsedTime / i_time;
+
+			Vector3 scale           = Vector3.Lerp( fromScale, i_toScale, elapsedRate );
+			transform.localScale    = scale;
+
+			yield return null;
+		}
+
+		transform.localScale    = i_toScale;
+	}
+
+} 
