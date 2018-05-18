a7d93667afcccc4936facac022cc4754b0cc2ca0
平井将也 <>
2018-05-19 00:16:27 +0900

    [add]コード追加
    #2 落ちる床のコードを追加

---
+++ Assets/OriginalObjectScripts/FallFloor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FallFloor : MonoBehaviour {
+
+	void OnCollisionEnter(Collision collision){
+		if(collision.gameObject.CompareTag("Player")){
+			Invoke("Fall",2);
+
+		}
+	}
+
+	void Fall (){
+		GetComponent<Rigidbody>().isKinematic = false;
+	}
+}
