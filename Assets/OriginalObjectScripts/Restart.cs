73c37b6187c1cbcde9aadf739774b2587c69c294
平井将也 <>
2018-05-24 01:45:21 +0900

    [add]リスタート機能を追加
    #5 このゲームにリスタート機能を追加

---
+++ Assets/OriginalObjectScripts/Restart.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Restart : MonoBehaviour {
+
+		// ★（追加）
+		// 先頭に「public」をつけること（ポイント）
+		public void OnRestartButtonClicked(){
+			SceneManager.LoadScene ("Main");
+		}
+	}
