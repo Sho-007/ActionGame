3065d872b23d1107612d7b5f016248a8ba185e74
平井将也 <>
2018-05-26 00:55:00 +0900

    [change]コードの仕様変更
    #6 エネミー死亡時の処理に発生する道をHP0になると同時に発生する仕様に変更

---
+++ Assets/OriginalObjectScripts/ObjectController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectController : MonoBehaviour {
+
+	public void EnemyController (){
+		int hp = 0;
+			
+	}
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (hp = 0){
+			
+		}
+	}
+}
