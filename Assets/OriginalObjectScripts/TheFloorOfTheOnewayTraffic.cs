abff524b1cd0f71432618742fbb5b73059694fa1
平井将也 <>
2018-05-29 04:16:09 +0900

    [add]一方通行の床の追加
    #4 操作キャラが移動する際にすり抜けた後に戻ることのできない一方通行の床のスクリプトを追加

---
+++ Assets/OriginalObjectScripts/TheFloorOfTheOnewayTraffic.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheFloorOfTheOnewayTraffic : MonoBehaviour {
+
+	public bool call_enter;
+	public bool call_exit;
+
+	private GameObject Parent;
+
+	void Start () {
+		Parent = transform.parent.gameObject;
+	}
+
+	private void OnTriggerEnter(Collider c) {
+		if (call_enter) {
+			Parent.SendMessage("OnChildTriggerEnter", c);
+		}
+	}
+
+	private void OnTriggerExit(Collider c) {
+		if (call_exit) {
+			Parent.SendMessage("OnChildTriggerExit", c);
+		}
+	}
+}
