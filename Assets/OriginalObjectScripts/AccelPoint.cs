0717d37238afd0cfe489d0e5ada8b5b53e19d5f2
平井将也 <>
2018-06-03 03:54:39 +0900

    [fix]脱字を修正
    #4 脱字によってコンソールに警告が表示されてたためバグを修正

--- Assets/OriginalObjectScripts/AccelPoint.cs
+++ Assets/OriginalObjectScripts/AccelPoint.cs
@@ -4,18 +4,21 @@ using UnityEngine;
  
 public class AccelPoint : MonoBehaviour {
 
+
+
+
 	public class MoveAnime {
 		private const float _RADIAN = 3.1415f / 180;
 		public static float[] GetBrake(uint frame, float distance) {
 			float a = 90.0f / frame;
 			float[] p = new float[frame];
-			for (uint i = 0; i < frame; i++) p[i] = Math.Sin (i * a * MoveAnime._RADIAN) * distance;
+			for (uint i = 0; i < frame; i++) p[i] = Mathf.Sin (i * a * MoveAnime._RADIAN) * distance;
 			return p;
 		}
 		public static float[] GetAccel(uint frame, float distance) {
 			float a = 90.0f / frame;
 			float[] p = new float[frame];
-			for (uint i = 0; i < frame; i++) p[i] = (1 - Math.Sin ((i * a + 90) * MoveAnime._RADIAN)) * distance;
+			for (uint i = 0; i < frame; i++) p[i] = (1 - Mathf.Sin ((i * a + 90) * MoveAnime._RADIAN)) * distance;
 			return p;
 		}
 	}
