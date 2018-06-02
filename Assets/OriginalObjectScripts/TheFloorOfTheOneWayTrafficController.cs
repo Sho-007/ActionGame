aed8fc2fc2aba9d880d10e6c77144ed94641b26e
平井将也 <>
2018-06-03 03:47:41 +0900

    [add]操作キャラの子オブジェクトにコード追加
    #2 操作キャラが一方通行の道をすり抜けるための判定を出すために子オブジェクトのコードを追加

---
+++ Assets/OriginalObjectScripts/TheFloorOfTheOneWayTrafficController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheFloorOfTheOneWayTrafficController: MonoBehaviour {
+
+	private BoxCollider fllorCollider;
+
+	void Start () {
+		fllorCollider = this.GetComponent<BoxCollider>();
+	}
+
+	private void OnChildTriggerEnter(Collider c) {
+		if (c.gameObject.tag == "TriggerCollider") {
+			fllorCollider.isTrigger = true;
+		}
+	}
+
+	private void OnTriggerExit(Collider c) {
+		if (c.gameObject.tag == "TriggerCollider") {
+			fllorCollider.isTrigger = false;
+		}
+	}
+}
