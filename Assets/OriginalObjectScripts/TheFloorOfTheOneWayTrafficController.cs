d8a76b16966f2a726fe4d35e4faacf78deec17a8
平井将也 <>
2018-06-04 03:16:19 +0900

    [add]レイヤーの追加
    #4 判定を無視するためにレイヤーを追加

--- Assets/OriginalObjectScripts/TheFloorOfTheOneWayTrafficController.cs
+++ Assets/OriginalObjectScripts/TheFloorOfTheOneWayTrafficController.cs
@@ -4,21 +4,25 @@ using UnityEngine;
 
 public class TheFloorOfTheOneWayTrafficController: MonoBehaviour {
 
-	private BoxCollider fllorCollider;
+	private int player_layer;
+	private int sliding_floor_layer;
+
 
 	void Start () {
-		fllorCollider = this.GetComponent<BoxCollider>();
+		//各レイヤーの情報取得
+		player_layer = LayerMask.NameToLayer("Player");
+		sliding_floor_layer = LayerMask.NameToLayer("SlidingFloor");
 	}
 
 	private void OnChildTriggerEnter(Collider c) {
 		if (c.gameObject.tag == "TriggerCollider") {
-			fllorCollider.isTrigger = true;
+			Physics.IgnoreLayerCollision (player_layer, sliding_floor_layer);
 		}
 	}
 
 	private void OnTriggerExit(Collider c) {
 		if (c.gameObject.tag == "TriggerCollider") {
-			fllorCollider.isTrigger = false;
+			Physics.IgnoreLayerCollision(player_layer, sliding_floor_layer, false);
 		}
 	}
 }
