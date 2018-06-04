08769e445cf1c5e93545e5dad02fab26f6f0f068
平井将也 <>
2018-06-05 00:12:30 +0900

    [add]コードを追加
    #3 ボタンを押すことでポーズ状態になるコードを追加

---
+++ Assets/OriginalObjectScripts/PauseScreenScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScreenScript : MonoBehaviour {
+
+	[SerializeField]
+	//ポーズした時に表示するUI
+	private GameObject pauaseUI;
+	//ポーズUIのインスタンス
+	private GameObject instancePauseUi;
+
+	//Update is called once per frame
+	void Update (){
+		if (Input.GetKeyDown ("q")) {
+			if (instancePauseUI == null) {
+				instancePauseUI = GameObject.Instantiate (pauseUI) as GameObject;
+				Time.timeScale = 0f;
+			} else {
+				Destroy (instancepauseUI);
+				Time.timeScale = 1f;
+			}
+		}
+	}
+}
