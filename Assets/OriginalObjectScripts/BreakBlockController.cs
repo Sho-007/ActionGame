93cd530d19e766d0101033aa538c0843d50192f7
平井将也 <>
2018-06-07 01:07:32 +0900

    [add]コードの追加
    #3 操作キャラが触れることでブロックが崩れるコードを追加

---
+++ Assets/OriginalObjectScripts/BreakBlockController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakBlockController : MonoBehaviour {
+
+	private GameObject Parent;
+
+	void Start()
+	{
+		Parent = transform.parent.gameObject;
+	}
+
+	private void OnTriggerEnter(Collider c)
+	{
+		Parent.SendMessage("OnChildTriggerEnter", c);
+	}
+}
---
+++ Assets/OriginalObjectScripts/ThroughBlockController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThroughBlockController : MonoBehaviour {
+
+	protected BoxCollider boxCollider;
+	protected List<Rigidbody> separateBlocks;
+
+	void Start()
+	{
+		boxCollider = GetComponent<BoxCollider>();
+		separateBlocks = GetComponentsInChildren<Rigidbody>().ToList();
+	}
+
+	private void OnChildTriggerEnter(Collider c)
+	{
+		if (c.gameObject.tag == "Player") {
+			boxCollider.enabled = false;
+
+			var random = new System.Random();
+			var min = -3;
+			var max = 3;
+			separateBlocks.ForEach(r => {
+				r.isKinematic = false;
+
+				var vect = new Vector3(random.Next(min, max), 0, random.Next(min, max));
+				r.AddForce(vect, ForceMode.Impulse);
+			});
+		}
+	}
+}
