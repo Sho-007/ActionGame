d63e21fffd86b01766e8b5ee1cb2bffbbc61b870
平井将也 <>
2018-06-07 23:12:26 +0900

    [change]コードの仕様変更
    #2 操作キャラを自動で追尾する弾が出る動作が操作キャラのコマンドとして扱われていたため仕様変更

--- Assets/OriginalObjectScripts/Bullet2.cs
+++ Assets/OriginalObjectScripts/Bullet2.cs
@@ -6,28 +6,28 @@ public class Bullet2 : MonoBehaviour {
 
 	public string name = "bullet2";
 	Bullet2 GameObject;
-	public float speed = 3f;
-
+	public float speed = 3f; //弾の速度
 
 	// Use this for initialization
 	void Start () {
 		//弾を3秒後に発射する
 		Destroy (this.gameObject,3);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		//弾を移動
-		if(Input.GetKeyDown("space")
+		if(EnemyTargetTrigger)
 			{
 				//runcherbulletにbulletのインスタンスを格納
-				GameObject runcherBullet = GameObject.Instantiate(bullet2) as GameObject; 
+				GameObject runcherBullet = GameObject.Instantiate("bullet2") as GameObject; 
 				//アタッチしているオブジェクト前方にbullet speedの速さで発射
 				runcherBullet.GetComponent<Rigidbody>().velocity = transform.forward * speed;
 				runcherBullet.transform.position = transform.position;
-			}
 
-		transform.Translate (transform.forward * Time.deltaTime * speed);
+			}
+			transform.Translate(transform.forward * Time.deltaTime * speed);
 		
 			}
 }
