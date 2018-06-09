4b9e6405bddbb0f24d579b367770f9d60b4d6940
平井将也 <>
2018-06-10 05:06:09 +0900

    [add]コードを追加
    #2 隕石が爆発する際にエフェクトを発生させてオブジェクトの判定をなくすようにコードに追加

---
+++ Assets/OriginalObjectScripts/ExplodeMetor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodeMetor : MonoBehaviour {
+
+
+	// On the explosion object.
+	void Start() {
+		var exp = GetComponent<ParticleSystem>();
+		exp.Play();
+		Destroy(gameObject, exp.duration);
+	}
+
+
+	void Explode() {
+		var exp = GetComponent<ParticleSystem>();
+		exp.Play();
+		Destroy(gameObject, exp.duration);
+	}
+
+	// Grenade explodes on impact.
+	void OnCollisionEnter(Collision coll) {
+		Explode();
+	}
+
+
+	// Possible projectile script.
+	public GameObject explosionPrefab;
+
+	void Update() {
+		RaycastHit hit;
+
+		if (Physics.Raycast (Camera.main.ScreenPointToRay (Input.mousePosition), out hit)) {
+			Instantiate (explosionPrefab, hit.point, Quaternion.identity);
+		}
+	}
+}
