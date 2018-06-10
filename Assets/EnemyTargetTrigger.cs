d5bc7f4b6c3115905aa7974cb707ce00adc278c7
平井将也 <>
2018-06-11 00:18:36 +0900

    [remove&add]コード削除&追加
    #1 コードを書いていたところ不具合が発生して書いていたコードがなくなったため削除

---
+++ Assets/EnemyTargetTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTargetTrigger : MonoBehaviour {
+
+
+	void Update(){
+	//　プレイヤーキャラクターを発見
+	if(col.tag == "Player") {
+		//　敵キャラクターの状態を取得
+		MoveEnemy.EnemyState state = GetComponentInParent <MoveEnemy> ().GetState ();
+		//　敵キャラクターが追いかける状態でなければ追いかける設定に変更
+		if (state != MoveEnemy.EnemyState.Chase) {
+			Debug.Log ("プレイヤー発見");
+			GetComponentInParent<MoveEnemy> ().SetState ("chase", col.transform);
+		}
+		}
+
+}
