39f0563d56a15d965e55c721166d7ddecc809769
平井将也 <>
2018-06-11 00:25:35 +0900

    [add]コードを追加
    #3 無敵アイテムを取った際にエフェクト発生し続けるようにコードを追加

--- Assets/OriginalObjectScripts/NoDamageItem.cs
+++ Assets/OriginalObjectScripts/NoDamageItem.cs
@@ -6,12 +6,18 @@ public class NoDamageItem : MonoBehaviour {
 
 	public GameObject Destroyeffect;
 	public AudioClip get_audio;
+	public GameObject NoDamageItem_get_effect;
+	public 
+
 	public GameObject NoDamageItem
 
 
 
-	void OnTriggerStay(Collider NoDamageItem){
-		PlayerHP
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "Player"{
+			Destroy(gameObject);
+			Instantiate(Destroyeffect,transform.position,transform.rotation);
+			AudioSource.P
 
 	void Update(){
 
@@ -21,7 +27,7 @@ public class NoDamageItem : MonoBehaviour {
 	if(other.gameObject.tag == "Player") {
 		Destroy (gameObject);
 		Instantiate (Destroyeffect, transform.position, transform.rotation);
-		AudioSource.PlayClipAtPoint (get_audio, Camera.main.transform.position);
+				AudioSource.PlayClipAtPoint (get_audio, Camera.main.transform.position);
 		StartCoroutine ("destroy_effect");
 	}
 }
