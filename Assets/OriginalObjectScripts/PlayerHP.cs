7e98c592e448031950e1daa583cc009eb2973502
平井将也 <>
2018-06-17 07:02:31 +0900

    [add]コードの追加
    #1 操作キャラのHPゲージを追加

--- Assets/OriginalObjectScripts/PlayerHP.cs
+++ Assets/OriginalObjectScripts/PlayerHP.cs
@@ -3,14 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerHP : MonoBehaviour {
+	//最大体力
+	public readonly int maxHP = 100;
+
+	//体力
+	public int HP;
+
+	//敵の攻撃力
+	public int EnemyAttack = 10;
+
+
 
 	// Use this for initialization
 	void Start () {
-		
+		//初期hpを最大にする
+		HP = maxHP;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (HP <= 0) {
+			Debug.Log ();
+		}
 	}
+
+	void OnTriggerEnter(){
+		//敵の攻撃で体力減少
+		HP -= EnemyAttack;
+		//HPを表示
+		Debug.Log (HP);
+	}
+}
 }
---
+++ Assets/PlayerHPBar.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHPBar : MonoBehaviour {
+
+
+	GameObject player;
+	PlayerHP hpcomp;
+	Slider hpslider;
+	private int hp;
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find("Player");
+		hpcomp = player.GetComponent <PlayerHP> ();
+		hpslider = GameObject.Find ("HPBar").GetComponent<Slider> ();
+		//最大hpの値
+		hp = 100;
+		//sliderのValueの値を最大値にする
+		hpslider.value = hp;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//PlayerHP内の変数HPをPyHPを使用
+		int PyHP = hpcomp.HP;
+		//Valueの値をPyHPにする
+		hpslider.value = PyHP;
+	}
+}
