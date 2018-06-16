a601e44b773c62028d3009dddefc24024b81f1aa
平井将也 <>
2018-06-17 07:05:59 +0900

    [changeコードの仕様変更
    #4 操作キャラを発見した際の挙動の仕様変更

--- Assets/EnemyTargetTrigger.cs
+++ Assets/EnemyTargetTrigger.cs
@@ -1,13 +1,19 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyTargetTrigger : MonoBehaviour {
 
 
+	void OnTriggerStay(Collision col){
+		//プレイヤーの発見
+		if (col.tag == "Player") {
+		}
+	}
+
+
 	void Update(){
 	//　プレイヤーキャラクターを発見
-	if(col.tag == "Player") {
+	if(collision.tag == "Player") {
 		//　敵キャラクターの状態を取得
 		MoveEnemy.EnemyState state = GetComponentInParent <MoveEnemy> ().GetState ();
 		//　敵キャラクターが追いかける状態でなければ追いかける設定に変更
@@ -15,6 +21,10 @@ public class EnemyTargetTrigger : MonoBehaviour {
 			Debug.Log ("プレイヤー発見");
 			GetComponentInParent<MoveEnemy> ().SetState ("chase", col.transform);
 		}
+		}
+
+		public void Setstate(){
+			
 		}
 
 }
