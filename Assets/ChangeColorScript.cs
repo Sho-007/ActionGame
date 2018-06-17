d1796424a8a9fb5f1b47044647627bb135b5c8ce
平井将也 <>
2018-06-18 06:38:35 +0900

    [add]コードを追加
    #1 ドロップアイテムの色が変わるコードを追加

---
+++ Assets/ChangeColorScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColorScript : MonoBehaviour {
+
+	public GameObject Cube
+
+	// Use this for initialization
+	void Start () {
+		//gameobujectの取得
+		Cube = GameObject.Find("CubeName");
+
+		//今の色コンソールに出力
+		Debug.Log(Cube.render.material.color);
+
+		//青色に変更
+
+
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
