ecccf1d90cdc5a1a9ac2e480e7630e5114d2be98
平井将也 <>
2018-06-20 03:00:22 +0900

    [change]コードの仕様変更
    #2 武器の先端から弾を出す。連射時と弾の生成の使用変更

--- Assets/OriginalObjectScripts/Bullet2.cs
+++ Assets/OriginalObjectScripts/Bullet2.cs
@@ -4,12 +4,33 @@ using System.Collections.Generic;
 using UnityEngine.AI;
 
 public class Bullet2 : MonoBehaviour {
-	
+
+	//弾のプレハブ　クローン
+	public GameObject bullet2;
+	GameObject  bulletClone;
+
+	//武器の先端
+	public Transform tip;
+
+
+	//弾速度
+	public float speed = 5000;
+
+	//連射時設定
+	private float time 0f;
+	public float interval = 0.3f;
+
+	void Start(){
+		
+	}
+
+
+
 	// Update is called once per frame
 	void Update () {
+		time += Time.deltaTime;
 		GameObject unitychan  = GameObject.Find("unitychan");
 		float speed = 1.0f
-		float step = Time.deltaTime*speed;
 		transform.position = Vector3.MoveTowards(transform.position, unitychan.transform,position, step);
 	}		
 
