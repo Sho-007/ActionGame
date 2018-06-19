3392378f548454bff551807c6b9b3a1a2ebafc21
平井将也 <>
2018-06-20 03:23:27 +0900

    [change]コードの仕様変更
    #6 色が変わった際に画面にコンソールに表示・カメラに入った際に色を変更するように変更

--- Assets/ChangeColorScript.cs
+++ Assets/ChangeColorScript.cs
@@ -1,13 +1,27 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ChangeColorScript : MonoBehaviour {
 
-	public GameObject Cube
+	public GameObject Cube;
+	Renderer CubeRenderer;
+	Material CubeMaterial;
+	Color CubeColor;
+
+	void awake(){
+		
+	}
 
 	// Use this for initialization
 	void Start () {
+
+		CubeRenderer = GetComponent<Renderer>;
+
+		CubeRenderer.material.color = Color.red
+
+
+
+
 		//gameobujectの取得
 		Cube = GameObject.Find("CubeName");
 
@@ -15,15 +29,15 @@ public class ChangeColorScript : MonoBehaviour {
 		Debug.Log(Cube.render.material.color);
 
 		//青色に変更
+		Cube.renderer.material.color = Color.blue;
 
-
-
-
+		//変更後のコンソールに出力
+		Debug.Log(Cube.renderer.material.color);
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Ray ray new Ray(Camera
 	}
 }
