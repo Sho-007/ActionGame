c3f92be4d7837bf3903c55e6e963e6d34241a814
平井将也 <>
2018-06-23 02:13:44 +0900

    [add]コードの追加
    #2 射撃攻撃を防ぐシールドアイテムのコードを追加

---
+++ Assets/ShieldItemScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldItemScript : MonoBehaviour {
+
+	//ぶつかってくるエネミーの射撃攻撃を防ぐ
+	void OnTriggerEnter(Collider other){
+		if (other.CompareTag ("EnemyBullets")) {
+			Destroy (other.gameObject);
+		}
+	}
+}
