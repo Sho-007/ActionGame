683c91ae82568b46c035fd24330ea3bed5ba0c0e
平井将也 <>
2018-06-23 02:14:16 +0900

    [add]コードの追加
    #3 拡散する弾のコードを追加

---
+++ Assets/Bullet3.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet3 : MonoBehaviour {
+
+	private GameObject target;
+
+
+
+	// Use this for initialization
+	void Start () {
+		//名前でオブジェクト特定
+		target = GameObject.Find("unitychan");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.GameObject.transform.LookAt (target.transform.position);
+	}
+}
