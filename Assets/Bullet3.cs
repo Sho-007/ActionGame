ed6804106e48cc70c42601969cf215df4486e3ba
平井将也 <>
2018-06-24 03:29:19 +0900

    [add]コードの追加
    #1 増殖する弾をプレハブから呼び、発生間隔、一定の位置で生成するコードを追加

--- Assets/Bullet3.cs
+++ Assets/Bullet3.cs
@@ -5,15 +5,31 @@ using UnityEngine;
 public class Bullet3 : MonoBehaviour {
 
 	private GameObject target;
-
+	//発生するオブジェクトをInspectorから指定するよう
+	public GameObject SpawnObject;
+	//発生間隔
+	public float interval = 3.0f;
 
 
 	// Use this for initialization
 	void Start () {
-		//名前でオブジェクト特定
+		//名前でオブジェクト特定b
 		target = GameObject.Find("unitychan");
+		StartCoroutine("Spawn");
 	}
-	
+
+	IEnumerator Spawn(){
+		while (true) {
+			//自分で付けた位置に、オブジェクトをインスタンス化してから生成する
+			Instantiate(SpawnObject,transform.position,Quaternion.identity);
+			yield return new WaitForSeconds (interval);
+		}
+	}
+
+
+
+
+
 	// Update is called once per frame
 	void Update () {
 		this.GameObject.transform.LookAt (target.transform.position);
