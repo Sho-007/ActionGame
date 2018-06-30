466afd5451e2a838c2c46c7e2099da75473b6935
平井将也 <>
2018-07-01 04:48:52 +0900

    [fix]コードの修正
    #17 脱字の修正

--- Assets/Original unitychan Scripts/PlayerAttack.cs
+++ Assets/Original unitychan Scripts/PlayerAttack.cs
@@ -6,21 +6,24 @@ public class PlayerAttack : MonoBehaviour {
 
 	private Animator anim;
 	public AudioClip punchSound;
+	Bullet2 player;
 	void Start () {
-
+		player.speed;
 		anim = GetComponent<Animator> ();	
 	}
 
 	void Update () {
 
 		// 地上にいるときに下入力した場合の処理
-		if (Input.GetAxis("Vertical") < -0.5f && charMotor.IsGrounded()) {
+		//if (Input.GetAxis("Vertical") < -0.5f && charMotor.IsGrounded()) {
+		if (Input.GetAxis ("Vertical") < -0.5f) {
 			// すり抜け床の上にいる場合はレイヤーマスク変更
-			int sliding_floor_layer = LayerMask.NameToLayer("SlidingFloor");
-			if (Physics.Raycast(transform.position, -transform.up, sliding_floor_layer)) {
-				int player_layer = LayerMask.NameToLayer("Player");
-				Physics.IgnoreLayerCollision(player_layer, sliding_floor_layer);
+			int sliding_floor_layer = LayerMask.NameToLayer ("SlidingFloor");
+			if (Physics.Raycast (transform.position, -transform.up, sliding_floor_layer)) {
+				int player_layer = LayerMask.NameToLayer ("Player");
+				Physics.IgnoreLayerCollision (player_layer, sliding_floor_layer);
 			}
+		}
 
 
 
@@ -32,7 +35,7 @@ public class PlayerAttack : MonoBehaviour {
 		// Qボタンを押すとパンチ
 		if (Input.GetKeyDown (KeyCode.Q)) {
 			anim.SetBool ("Jab", true);
-			AudioSource.PlayClipAtPoint (punchSound, Camera.main.transform.position);
+			AudioSource.PlayClipAtPoint (punchSound, UnityEngine.Camera.main.transform.position);
 		}
 
 		// Eボタンを押すとハイキック
