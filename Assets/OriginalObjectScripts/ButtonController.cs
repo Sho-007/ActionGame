2e2aea94b87bed7f3013776bc89ee77bad913ac5
平井将也 <>
2018-07-01 04:37:22 +0900

    [add]コードの追加
    #8 Unityからクラスを引っ張るためにコードを追加

--- Assets/OriginalObjectScripts/ButtonController.cs
+++ Assets/OriginalObjectScripts/ButtonController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.SceneManegement;
+using UnityEngine.SceneManagement;
+using UnityEngine;
 
 public class ButtonController : MonoBehaviour {
 
