2e54d44cf07e9479e9befedfec9c79cbf6ac41b3
平井将也 <>
2018-07-06 01:11:58 +0900

    [add]コードの追加
    #1 unityから引き出すクラスの追加

--- Assets/Original unitychan Scripts/PlayerDamageMeausurement.cs
+++ Assets/Original unitychan Scripts/PlayerDamageMeausurement.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerDamageMeausurement : MonoBehaviour {
 
+
+
+
 	// Use this for initialization
 	void Start () {
 		
--- Assets/OriginalEnemyObjectScripts/Bullet2.cs
+++ Assets/OriginalEnemyObjectScripts/Bullet2.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.AI;
+using System;
 
 public class Bullet2 : MonoBehaviour {
 
