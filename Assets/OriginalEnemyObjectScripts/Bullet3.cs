d3efb6b387276fed8ae6384d214b619d53169b87
平井将也 <>
2018-07-06 01:12:59 +0900

    [fix]コードの修正
    #2 前述したコードとかぶったためにエラーを出したために修正

--- Assets/OriginalEnemyObjectScripts/Bullet3.cs
+++ Assets/OriginalEnemyObjectScripts/Bullet3.cs
@@ -32,6 +32,6 @@ public class Bullet3 : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		this.GameObject.transform.LookAt (target.transform.position);
+		this.gameObject.transform.LookAt (target.transform.position);
 	}
 }
