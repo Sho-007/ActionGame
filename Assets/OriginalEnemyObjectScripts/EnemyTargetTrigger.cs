bea94e153d29480642fdb8cc95fcdc2947640d22
平井将也 <>
2018-07-08 00:59:47 +0900

    [change]コードの仕様変更
    #2 エネミーの操作キャラ捕捉範囲のコードにエネミーの動きのコードを呼ぶように仕様変更

--- Assets/OriginalEnemyObjectScripts/EnemyTargetTrigger.cs
+++ Assets/OriginalEnemyObjectScripts/EnemyTargetTrigger.cs
@@ -15,7 +15,7 @@ public class EnemyTargetTrigger : MonoBehaviour {
 	//　プレイヤーキャラクターを発見
 	if(collision.tag == "Player") {
 		//　敵キャラクターの状態を取得
-		MoveEnemy.EnemyState state = GetComponentInParent <MoveEnemy> ().GetState ();
+			MoveEnemy.EnemyState state = GetComponentInParent <MoveEnemy>().State ();
 		//　敵キャラクターが追いかける状態でなければ追いかける設定に変更
 		if (state != MoveEnemy.EnemyState.Chase) {
 			Debug.Log ("プレイヤー発見");
