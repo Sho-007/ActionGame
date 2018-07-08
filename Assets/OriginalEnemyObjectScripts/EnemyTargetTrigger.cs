4ac49fa4e08bfe266f43078b4c550c2e7b5ec7d1
平井将也 <>
2018-07-09 05:49:36 +0900

    [change]
    #1と同じ

--- Assets/OriginalEnemyObjectScripts/EnemyTargetTrigger.cs
+++ Assets/OriginalEnemyObjectScripts/EnemyTargetTrigger.cs
@@ -15,9 +15,9 @@ public class EnemyTargetTrigger : MonoBehaviour {
 	//　プレイヤーキャラクターを発見
 	if(collision.tag == "Player") {
 		//　敵キャラクターの状態を取得
-			MoveEnemy.EnemyState state = GetComponentInParent <MoveEnemy>().State ();
+			MoveEnemy.SpaceFighterEnemyState state = GetComponentInParent <MoveEnemy>().State ();
 		//　敵キャラクターが追いかける状態でなければ追いかける設定に変更
-		if (state != MoveEnemy.EnemyState.Chase) {
+		if (state != MoveEnemy.SpaceFighterEnemyState.Chase) {
 			Debug.Log ("プレイヤー発見");
 			GetComponentInParent<MoveEnemy> ().SetState ("chase", col.transform);
 		}
