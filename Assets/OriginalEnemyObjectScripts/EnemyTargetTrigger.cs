39612a96f04ee51bbafbf8d461b5c89d50900891
平井将也 <>
2018-07-11 05:29:56 +0900

    [add]コードの追加
    #1 抜けていたコードを追加

--- Assets/OriginalEnemyObjectScripts/EnemyTargetTrigger.cs
+++ Assets/OriginalEnemyObjectScripts/EnemyTargetTrigger.cs
@@ -4,18 +4,27 @@ using UnityEngine;
 public class EnemyTargetTrigger : MonoBehaviour {
 
 
-	void OnTriggerStay(Collision col){
+	GameObject tagObjects;
+
+	void Start(){
+	}
+
+
+
+
+	void OnTriggerStay(Collision collision){
 		//プレイヤーの発見
-		if (col.tag == "Player") {
+		if (gameObject.tag == "Player") {
 		}
 	}
 
 
 	void Update(){
 	//　プレイヤーキャラクターを発見
-	if(collision.tag == "Player") {
+		if(gameObject.tag == "Player") {
 		//　敵キャラクターの状態を取得
 			MoveEnemy.SpaceFighterEnemyState state = GetComponentInParent <MoveEnemy>().State ();
+
 		//　敵キャラクターが追いかける状態でなければ追いかける設定に変更
 		if (state != MoveEnemy.SpaceFighterEnemyState.Chase) {
 			Debug.Log ("プレイヤー発見");
