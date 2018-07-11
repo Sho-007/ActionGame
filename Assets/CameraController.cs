7e6a07324c09e22f368c4e05028abba2a045b41b
平井将也 <>
2018-07-12 02:35:00 +0900

    [add]コードの追加 #1 新しくUNITYに対応したコードを追加

---
+++ Assets/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraController : MonoBehaviour {
+
+
+	public GameObject unitychan;
+	public GameObject mainCamera;
+
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		mainCamera = UnityEngine.Camera.main.gameObject;
+		unitychan = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = unitychan.transform.position;
+	}
+}
+
