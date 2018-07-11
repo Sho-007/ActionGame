912ed46002b47ff0320497bd4abfaec0753f0d0a
平井将也 <>
2018-07-12 02:36:54 +0900

    [change]コードを変更
    #3 死亡時にカメラのアングルの仕様変更

--- Assets/OriginalObjectScripts/DamageReturnArea.cs
+++ Assets/OriginalObjectScripts/DamageReturnArea.cs
@@ -1,28 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
  
 public class DamageReturnArea : MonoBehaviour {
  
-    public float power;
- 
-    private Transform returnPoint;
- 
-    void Start() {
-        returnPoint = transform.Find("ReturnPoint");
-    }
- 
-    private void OnTriggerEnter(Collider collider) {
-        string tag = TagUtility.getParentTagName(collider.gameObject);
- 
-        if (gameObject.tag == "Player") {
-			collider.GetComponent<Player>();forceDownDamage(this);
-            StartCoroutine("returnCharacter", collider.gameObject);
-        }
-    }
- 
-    private IEnumerator returnCharacter(GameObject character) {
-        yield return new WaitForSeconds(1f);
- 
-        character.transform.position = returnPoint.position;
-    }
+	public float power;
+
+	private Transform returnPoint;
+	private CameraController cameraController;
+
+	void Start() {
+		returnPoint = transform.Find("ReturnPoint");
+	}
+
+	void Update() {
+		cameraController = GameObject.FindGameObjectWithTag("PlayerManager").GetComponent<CameraController>();
+	}
+
+	private void OnTriggerEnter(Collider c) {
+		string tag = TagUtility.getParentTagName(c.gameObject);
+
+		if (tag == "Player") {
+			c.GetComponent<Player>().forceDownDamage(this);
+			StartCoroutine("returnCharacter", c.gameObject);
+
+			// カメラアングルを固定
+			cameraController.mainCamera = true;
+		}
+	}
+
+	private IEnumerator returnCharacter(GameObject character) {
+		yield return new WaitForSeconds(1f);
+
+		cameraController.mainCamera = false;
+		character.transform.position = returnPoint.position;
+	}
 }
