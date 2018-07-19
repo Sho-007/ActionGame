e3e018ae661c0c8af0406dc4c0b7547c612be814
平井将也 <>
2018-07-20 01:53:06 +0900

    [add]コードの追加
    #2 特定のエネミーの初期位置・目的地の設定の追加

--- Assets/SetPositionEnemy.cs
+++ Assets/SetPositionEnemy.cs
@@ -4,8 +4,24 @@ using UnityEngine;
 
 public class SetPositionEnemy : MonoBehaviour {
 
+	//出現戦闘機エネミーの初期位置
+	private Vector3 startPosition;
+
+	//出現エネミーの目的地
+	private Vector3 destination;
+
+
+
+
 	// Use this for initialization
 	void Start () {
+		//スタートポジションの設定
+		startPosition = transform.position;
+		SetDestinaition(transform.position);
+	}
+
+
+	public void CreateRandomPosition(){
 		
 	}
 	
