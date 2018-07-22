41d507f47dae2ed905dfe7a907d2edaae5fbd6b0
平井将也 <>
2018-07-23 00:40:49 +0900

    [change]コードの仕様変更
    #5 戦闘機エネミーに到着フラグと目的地に到着したかどうかのフラグによって速度が変わるコードの仕様変更

--- Assets/BlenderBuildObjects/MoveEnemy.cs
+++ Assets/BlenderBuildObjects/MoveEnemy.cs
@@ -15,6 +15,10 @@ public class MoveEnemy : MonoBehaviour {
 	private Vector3 velocity;
 	//　移動方向
 	private Vector3 direction;
+	//到着フラグ
+	private bool arrived;
+	//スタート位置
+	private Vector3 startPosition;
 
 	// Use this for initialization
 	void Start () {
@@ -22,10 +26,13 @@ public class MoveEnemy : MonoBehaviour {
 		animator = GetComponent <Animator> ();
 		destination = new Vector3 (25f, 0f, 25f);
 		velocity = Vector3.zero;
+		arrived = false;
+		startPosition = transform.position;
 	}
 
 	// Update is called once per frame
 	void Update () {
+		if(!arrived){
 		if(enemyController.isGrounded) {
 			velocity = Vector3.zero;
 			animator.SetFloat("Speed", 2.0f);
@@ -35,6 +42,13 @@ public class MoveEnemy : MonoBehaviour {
 			Debug.Log (destination);
 		}
 		velocity.y += Physics.gravity.y * Time.deltaTime;
-		enemyController.Move(velocity * Time.deltaTime);	
+		enemyController.Move(velocity * Time.deltaTime);
+
+		//目的地に到着したかどうかのフラグ
+			if (Vector3.Distance (transform.position, destination) < 0.5f) {
+				arrived = true;
+				animator.SetFloat ("Speed", 0.0f);
+			}
+		}
 	}
 }
