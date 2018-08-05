05af7d0754a8fd362d29957b87e89ac2d753abc8
平井将也 <>
2018-08-06 00:52:11 +0900

    [revert]変更取り消し
    #1 unity更新によるエラーと考えていたが問題は別のコードだったため変更取り消し

--- Assets/BlenderBuildObjects/MoveEnemy.cs
+++ Assets/BlenderBuildObjects/MoveEnemy.cs
@@ -32,23 +32,34 @@ public class MoveEnemy : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		if(!arrived){
-		if(enemyController.isGrounded) {
+		if(state == EnemyState.Walk || state == EnemyState.Chase) {
+		//　キャラクターを追いかける状態であればキャラクターの目的地を再設定
+		if (state == EnemyState.Chase) {
+			setPosition.SetDestination (playerTransform.position);
+		}
+		if (enemyController.isGrounded) {
 			velocity = Vector3.zero;
-			animator.SetFloat("Speed", 2.0f);
-			direction = (destination - transform.position).normalized;
-			transform.LookAt(new Vector3(destination.x, transform.position.y, destination.z));
-			velocity = direction * FlyghtSpeed;
-			Debug.Log (destination);
+			animator.SetFloat ("Speed", 2.0f);
+			direction = (setPosition.GetDestination () - transform.position).normalized;
+			transform.LookAt (new Vector3 (setPosition.GetDestination ().x, transform.position.y, setPosition.GetDestination ().z));
+			velocity = direction * walkSpeed;
 		}
-		velocity.y += Physics.gravity.y * Time.deltaTime;
-		enemyController.Move(velocity * Time.deltaTime);
-
-		//目的地に到着したかどうかのフラグ
-			if (Vector3.Distance (transform.position, destination) < 0.5f) {
-				arrived = true;
-				animator.SetFloat ("Speed", 0.0f);
-			}
+ 
+		//　目的地に到着したかどうかの判定
+		if (Vector3.Distance (transform.position, setPosition.GetDestination ()) < 0.7f) {
+			SetState ("wait");
+			animator.SetFloat ("Speed", 0.0f);
 		}
+	//　到着していたら一定時間待つ
+	} else if(state == EnemyState.Wait) {
+		elapsedTime += Time.deltaTime;
+ 
+		//　待ち時間を越えたら次の目的地を設定
+		if(elapsedTime > waitTime) {
+			SetState ("walk");
+		}		
 	}
+	velocity.y += Physics.gravity.y * Time.deltaTime;
+	enemyController.Move (velocity * Time.deltaTime);
+}
 }
