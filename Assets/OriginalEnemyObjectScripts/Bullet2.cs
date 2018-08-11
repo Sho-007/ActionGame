ff59e7419e23cfd067b881f609856e7075412b98
平井将也 <>
2018-08-12 02:15:33 +0900

    [add]コードの追加
    #4 弾がZ軸方向に進むコードの追加

--- Assets/OriginalEnemyObjectScripts/Bullet2.cs
+++ Assets/OriginalEnemyObjectScripts/Bullet2.cs
@@ -14,14 +14,14 @@ public class Bullet2 : MonoBehaviour {
 
 
 	//弾速度
-	public float speed = 5000;
+	public float speed;
 
 	//連射時設定
 	private float time = 0f;
 	public float interval = 0.3f;
 
 	void Start(){
-		
+		transform.Translate(transform.forward * Time.deltaTime * speed);
 	}
 
 
