15949f288efb7f7285dc6f3f0d2404d4995735a4
平井将也 <>
2018-08-16 00:56:39 +0900

    [add]コードの追加
    #4 追従カメラの追加

---
+++ Assets/OriginalGameSystemOptionScripts/NormalFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalFollow : MonoBehaviour {
+
+	Vector3 diff;
+	//追従ターゲットパラメータ
+	public GameObject target;
+	public float followSpeed;
+
+	// Use this for initialization
+	void Start () {
+		diff = target.transform.position - transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = Vector3.Lerp(transform.position,target.transform.position - diff,Time.deltaTime * followSpeed);
+	}
+}
