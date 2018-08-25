59c983a477ee92d98ca3137c96dfc982703a6898
平井将也 <>
2018-08-26 00:48:51 +0900

    [fix]コードの修正
    #4 誤字の修正

--- Assets/OriginalGameSystemOptionScripts/LifePanel.cs
+++ Assets/OriginalGameSystemOptionScripts/LifePanel.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LifePanel : MonoBehaviour {
-	public GameObject() icons;
+	public GameObject[] icons;
 
 	public void UpdateLife (int life){
 		for (int i = 0; i < icons.Length; i++){
 
 		
-		if (i < life) icons(i)f.SetActive(true);
-		else icons(i).SetActive(false);
+		if (i < life) icons[i].SetActive(true);
+		else icons[i].SetActive(false);
 		}
 	}
 }
