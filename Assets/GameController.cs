4d6379fdca1207273b0e064f6143787b3bb55651
平井将也 <>
2018-08-26 00:47:12 +0900

    [add]コードの追加
    #1 ライフとスコアテキストの管理をするコードの追加

---
+++ Assets/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+	public PlayerController player;
+	//スコアラベルの参照
+	public Text ScoreLabel;
+	public LifePanel lifePanel;
+	
+	void Update () {
+		//スコアラベルを更新
+		int score = CalcScore();
+		//テキストの更新
+		ScoreLabel.text = "Score : " + score + "m";
+
+		//ライフパネルを更新
+		lifePanel.UpdateLife(player.Life());
+
+	}
+	int CalcScore(){
+		//unitychanの走行距離をスコアとする
+		return (int)player.transform.position.z;
+	}
+}
+
