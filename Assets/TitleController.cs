cefe3e30ac1151f0e378fa157d3baf60509afc0f
平井将也 <>
2018-08-26 22:40:47 +0900

    [add]コードの追加
    #2 タイトル画面のコードにハイスコアの表示とメインステージへの移行を追加

---
+++ Assets/TitleController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleController : MonoBehaviour {
+
+	public Text highScoreLabel;
+
+	public void Start(){
+		//ハイスコアを表示
+		highScoreLabel.text = "High Score : " + PlayerPrfs.GetInt("HighScore") + "m"
+	}
+
+	public void OnStartButtonClicked(){
+
+		Application.LoadLevel("Main");
+	}
+}
