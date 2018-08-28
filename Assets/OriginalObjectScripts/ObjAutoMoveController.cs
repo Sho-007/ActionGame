ef37de040c63a52efcf7e8a58c4cf9973ad3192c
平井将也 <>
2018-08-29 00:07:57 +0900

    [add]コードの追加
    #2 オブジェクトの上下するコードの追加

---
+++ Assets/OriginalObjectScripts/ObjAutoMoveController.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ObjAutoMoveController : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(transform.position.x, Mathf.PingPong(Time.time,10), transform.position.z);
+	}
+}
