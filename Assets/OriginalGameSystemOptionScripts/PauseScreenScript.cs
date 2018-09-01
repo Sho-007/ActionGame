a928b240a9b93d125de3d9701d3c991319dbaef1
平井将也 <>
2018-09-01 22:30:59 +0900

    [change&remove]コードの仕様変更&削除
    #4 キー入力でポーズ画面の表示するコードを書いたが、エラー解決のために削除

--- Assets/OriginalGameSystemOptionScripts/PauseScreenScript.cs
+++ Assets/OriginalGameSystemOptionScripts/PauseScreenScript.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class PauseScreenScript : MonoBehaviour {
 
+	
 	[SerializeField]
 	//ポーズした時に表示するUI
 	private GameObject pauaseUI;
 	//ポーズUIのインスタンス
 	private GameObject instancePauseUI;
 
+	
+
 	//Update is called once per frame
 	void Update (){
-		if (Input.GetKeyDown ("h")) {
+		if (Input.GetKeyDown (KeyCode.F)) {
 			if (instancePauseUI == null) {
 				instancePauseUI = GameObject.Instantiate (pauaseUI) as GameObject;
 				Time.timeScale = 0f;
