fccdff9869d8f1924d32f7b154f1ea213d5c2169
平井将也 <>
2018-10-04 01:53:50 +0900

    [add]コードの追加
    #3 わかるようにする為の説明文追加

--- Assets/OriginalGameSystemOptionScripts/LifePanel.cs
+++ Assets/OriginalGameSystemOptionScripts/LifePanel.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class LifePanel : MonoBehaviour {
 	public GameObject[] icons;
 
+	//ライフに応じてスプライトを出し分ける
 	public void UpdateLife (int life){
 		for (int i = 0; i < icons.Length; i++){
 
